6ffd865ae5c65feec8193207ac101124e1940d79
tranduc <tranduc.bk.1905@gmail.com>
2018-07-18 16:22:47 +0700

    Chapter 8 - transforming practice

---
+++ 08-transforming-operators-in-practice/GitFeed/ActivityController.swift
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2016-present Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Kingfisher
+
+class ActivityController: UITableViewController {
+
+  private let repo = "ReactiveX/RxSwift"
+
+  private let events = Variable<[Event]>([])
+  private let lastModified = Variable<NSString?>(nil)
+  private let bag = DisposeBag()
+  
+  private let eventsFileUrl = cachedFileURL("events.plist")
+  private let modifiedFileURL = cachedFileURL("modified.txt")
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = repo
+
+    self.refreshControl = UIRefreshControl()
+    let refreshControl = self.refreshControl!
+
+    refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+    refreshControl.tintColor = UIColor.darkGray
+    refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+    refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+
+    refresh()
+    
+    let eventsArray = (NSArray(contentsOf: eventsFileUrl) as? [[String : Any]]) ?? []
+    events.value = eventsArray.compactMap(Event.init)
+    
+    lastModified.value = try? NSString(contentsOf: modifiedFileURL, encoding: String.Encoding.utf8.rawValue)
+  }
+
+  @objc func refresh() {
+    DispatchQueue.global(qos: .background).async { [weak self] in
+      guard let strongSelf = self else { return }
+      strongSelf.fetchEvents(repo: strongSelf.repo)
+    }
+  }
+
+  func fetchEvents(repo: String) {
+    let response = Observable.from(["https://api.github.com/search/repositories?q=language:swift&per_page=5"])
+      .map { url -> URL in
+        return URL(string: url)!
+      }
+      .flatMap { url -> Observable<Any> in
+        return URLSession.shared.rx.json(url: url)
+      }
+      .flatMap { responses -> Observable<String> in
+        guard let responses = responses as? [String: Any],
+        let items = responses["items"] as? [[String: Any]] else {
+          return Observable.empty()
+        }
+        return Observable.from(items.compactMap { $0["full_name"] as? String })
+      }
+      .map { urlString -> URL in
+        return URL(string: "https://api.github.com/repos/\(urlString)/events?per_page=5")!
+      }
+      .map { [weak self] url -> URLRequest in
+        var request = URLRequest(url: url)
+        if let modifiedHeader = self?.lastModified.value {
+          request.addValue(modifiedHeader as String, forHTTPHeaderField: "Last-Modified")
+        }
+        return request
+      }
+      .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+        return URLSession.shared.rx.response(request: request)
+      }
+      .share(replay: 1, scope: .whileConnected)
+    
+    response
+      .filter { response, _ in
+        return 200..<300 ~= response.statusCode
+      }
+      .map { _, data -> [[String: Any]] in
+        guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+          let result = jsonObject as? [[String: Any]] else {
+            return []
+        }
+        return result
+      }
+      .filter { objects in return objects.count > 0}
+      .map { objects in
+        return objects.compactMap(Event.init)
+      }
+      .subscribe(onNext: { [weak self] newEvents in
+        self?.processEvents(newEvents)
+      })
+      .disposed(by: bag)
+    
+    response
+      .filter { response, _ in
+        return 200..<400 ~= response.statusCode
+      }
+      .flatMap { response, _ -> Observable<NSString> in
+        guard let value = response.allHeaderFields["Last-Modified"] as? NSString else {
+          return Observable.empty()
+        }
+        return Observable.just(value)
+      }
+      .subscribe(onNext: { [weak self] modifiedHeader in
+        guard let `self` = self else { return }
+        self.lastModified.value = modifiedHeader
+        try? modifiedHeader.write(to: self.modifiedFileURL, atomically: true, encoding: String.Encoding.utf8.rawValue)
+      })
+      .disposed(by: bag)
+  }
+  
+  private func processEvents(_ newEvents: [Event]) {
+    var updatedEvents = newEvents + events.value
+    if updatedEvents.count > 50 {
+      updatedEvents = Array<Event>(updatedEvents.prefix(upTo: 50))
+    }
+    events.value = updatedEvents
+    DispatchQueue.main.async {
+      self.tableView.reloadData()
+      self.refreshControl?.endRefreshing()
+    }
+    
+    let eventsArray = updatedEvents.map { $0.dictionary } as NSArray
+    eventsArray.write(to: eventsFileUrl, atomically: true)
+  }
+
+  // MARK: - Table Data Source
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return events.value.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let event = events.value[indexPath.row]
+
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    cell.textLabel?.text = event.name
+    cell.detailTextLabel?.text = event.repo + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
+    cell.imageView?.kf.setImage(with: event.imageUrl, placeholder: UIImage(named: "blank-avatar"))
+    return cell
+  }
+}
+
+func cachedFileURL(_ fileName: String) -> URL {
+  return FileManager.default
+    .urls(for: .cachesDirectory, in: .allDomainsMask)
+    .first!
+    .appendingPathComponent(fileName)
+}
