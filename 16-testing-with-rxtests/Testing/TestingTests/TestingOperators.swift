ff3919e508c6f1f62705c09bcc9ddd73aca95be1
tranduc <tranduc.bk.1905@gmail.com>
2018-08-07 18:22:40 +0700

    Chapter 16 - final.

--- 16-testing-with-rxtests/Testing/TestingTests/TestingOperators.swift
+++ 16-testing-with-rxtests/Testing/TestingTests/TestingOperators.swift
@@ -32,11 +32,91 @@ class TestingOperators : XCTestCase {
 
   override func setUp() {
     super.setUp()
+    scheduler = TestScheduler(initialClock: 0)
   }
 
   override func tearDown() {
-
+    scheduler.scheduleAt(1000) {
+      self.subscription.dispose()
+    }
 
     super.tearDown()
   }
+  
+  func testAmb() {
+    let observer = scheduler.createObserver(String.self)
+    
+    let observableA = scheduler.createHotObservable([
+        next(100, "A"),
+        next(200, "B"),
+        next(300, "C"),
+      ])
+    
+    let observableB = scheduler.createHotObservable([
+        next(90, "1"),
+        next(200, "2"),
+        next(300, "3")
+      ])
+    
+    let ambObserable = observableA.amb(observableB)
+    
+    scheduler.scheduleAt(0) {
+      self.subscription = ambObserable.subscribe(observer)
+    }
+    scheduler.start()
+    let results = observer.events.map {
+      $0.value.element
+    }
+    
+    XCTAssertEqual(results, ["1", "2", "3",])
+  }
+  
+  func testFilter() {
+    let observer = scheduler.createObserver(Int.self)
+    let observable = scheduler.createHotObservable([
+        next(100, 1),
+        next(200, 2),
+        next(300, 3),
+        next(400, 2),
+        next(500, 1)
+      ])
+    
+    let filterObservable = observable.filter {
+      $0 < 3
+    }
+    
+    scheduler.scheduleAt(0) {
+      self.subscription = filterObservable.subscribe(observer)
+    }
+    
+    scheduler.start()
+    
+    let results = observer.events.map {
+      $0.value.element
+    }
+    
+    XCTAssertEqual(results, [1,2,2,1])
+  }
+  
+  func testToArray() {
+    let scheduler = ConcurrentDispatchQueueScheduler(qos: .default)
+    let toArrayObservable = Observable.of(1,2).subscribeOn(scheduler)
+    XCTAssertEqual(try! toArrayObservable.toBlocking().toArray(), [1,2])
+  }
+  
+  func testToArrayMaterialized() {
+    let scheduler = ConcurrentDispatchQueueScheduler(qos: .default)
+    let toArrayObservable = Observable.of(1,2).subscribeOn(scheduler)
+    let result = toArrayObservable
+      .toBlocking()
+      .materialize()
+    switch result {
+    case .completed(elements: let elements):
+      XCTAssertEqual(elements, [1,2])
+    case .failed(elements: _, error: let error):
+      XCTFail(error.localizedDescription)
+    }
+    XCTAssertEqual(try! toArrayObservable.toBlocking().toArray(), [1,2])
+  }
+  
 }
--- 16-testing-with-rxtests/Testing/TestingTests/TestingViewModel.swift
+++ 16-testing-with-rxtests/Testing/TestingTests/TestingViewModel.swift
@@ -33,6 +33,55 @@ class TestingViewModel : XCTestCase {
 
   override func setUp() {
     super.setUp()
-
+    viewModel = ViewModel()
+    scheduler = ConcurrentDispatchQueueScheduler(qos: .default)
+    
+  }
+  
+  func testColorIsRedWhenHexStringIsFF0000_async() {
+    let disposeBag = DisposeBag()
+    let expect = expectation(description: #function)
+    var result: UIColor!
+    
+    viewModel.color.asObservable()
+      .skip(1)
+      .subscribe(onNext: {
+        result = $0
+        expect.fulfill()
+      })
+      .disposed(by: disposeBag)
+    
+    viewModel.hexString.value = "#ff0000"
+    waitForExpectations(timeout: 1) { error in
+      if let error = error {
+        XCTFail(error.localizedDescription)
+        return
+      }
+      XCTAssertEqual(.red, result)
+    }
+  }
+  
+  func testColorIsRedWhenHexStringIsFF0000() {
+    let colorObservable = viewModel.color.asObservable().subscribeOn(scheduler)
+    viewModel.hexString.value = "#ff0000"
+    let result = try! colorObservable.toBlocking(timeout: 1).first()
+    XCTAssertEqual(result, .red)
+  }
+  
+  func testRgbIs010WhenHexStringIs00FF00() {
+    let rgbObservable = viewModel.rgb.asObservable().subscribeOn(scheduler)
+    viewModel.hexString.value = "#00ff00"
+    let result = try! rgbObservable.toBlocking(timeout: 1).first()!
+    
+    XCTAssertEqual(0 * 255, result.0)
+    XCTAssertEqual(1 * 255, result.1)
+    XCTAssertEqual(0 * 255, result.2)
+  }
+  
+  func testColorNameIsRayWenderlichGreenWhenHexStringIs006636() {
+    let colorNameObservable = viewModel.colorName.asObservable().subscribeOn(scheduler)
+    viewModel.hexString.value = "#006636"
+    let result = try! colorNameObservable.toBlocking(timeout: 1).first()!
+    XCTAssertEqual("rayWenderlichGreen", result)
   }
 }
